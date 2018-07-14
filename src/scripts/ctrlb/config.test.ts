bf693c60ba51726ba9868433e0a45eca59f5008f
notomo <notomo.motono@gmail.com>
2018-07-15 08:19:15 +0900

    [add]more config test

--- src/scripts/ctrlb/config.test.ts
+++ src/scripts/ctrlb/config.test.ts
@@ -15,6 +15,21 @@ describe("Config", () => {
     expect(host).toBe(validHost);
   });
 
+  it("saveHost sets host if valid", async () => {
+    const validHost = "127.0.0.1:8888";
+
+    const setConfig = jest.fn();
+    const ConfigStorageClass = jest.fn<ConfigStorage>(() => ({
+      set: setConfig
+    }));
+    const storage = new ConfigStorageClass();
+    const config = new Config(storage);
+
+    await config.saveHost(validHost);
+
+    expect(setConfig).toBeCalledWith({ host: validHost });
+  });
+
   ["invalid:invalid:invalid", "invalid", "127.0.0.2"].forEach(invalidHost => {
     it(`getHost throws error if invalidHost: "${invalidHost}"`, async () => {
       const ConfigStorageClass = jest.fn<ConfigStorage>(() => ({
@@ -24,6 +39,16 @@ describe("Config", () => {
       const config = new Config(storage);
 
       expect(config.getHost()).rejects.toEqual(new ValidateError(invalidHost));
+    });
+
+    it(`saveHost throws error if invalidHost: "${invalidHost}"`, async () => {
+      const ConfigStorageClass = jest.fn<ConfigStorage>(() => ({}));
+      const storage = new ConfigStorageClass();
+      const config = new Config(storage);
+
+      expect(config.saveHost(invalidHost)).rejects.toEqual(
+        new ValidateError(invalidHost)
+      );
     });
   });
 });
