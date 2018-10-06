affaacad307fa5f5659116682b918f211b5c6185
notomo <notomo.motono@gmail.com>
2018-10-06 21:47:17 +0900

    Shorten webstore command
    - version 0.0.6 for deploy test

--- deploy.js
+++ deploy.js
@@ -7,17 +7,16 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CLIENT_ID = process.env.CLIENT_ID;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
-const webstoreCommand = "./node_modules/.bin/webstore";
 const version = JSON.parse(readFileSync("./dist/manifest.json"))["version"];
 const zipFilePath = `./ext/chrome/ctrlb-${version}.zip`;
 
 exec(
-  `${webstoreCommand} upload --source ${zipFilePath} --extension-id ${EXTENSION_ID} --client-secret ${CLIENT_SECRET} --client-id ${CLIENT_ID} --refresh-token ${REFRESH_TOKEN}`
+  `webstore upload --source ${zipFilePath} --extension-id ${EXTENSION_ID} --client-secret ${CLIENT_SECRET} --client-id ${CLIENT_ID} --refresh-token ${REFRESH_TOKEN}`
 )
   .then(({ stdout, stderr }) => {
     console.log(stdout);
     return exec(
-      `${webstoreCommand} publish --extension-id ${EXTENSION_ID} --client-secret ${CLIENT_SECRET} --client-id ${CLIENT_ID} --refresh-token ${REFRESH_TOKEN}`
+      `webstore publish --extension-id ${EXTENSION_ID} --client-secret ${CLIENT_SECRET} --client-id ${CLIENT_ID} --refresh-token ${REFRESH_TOKEN}`
     );
   })
   .then(({ stdout, stderr }) => {
