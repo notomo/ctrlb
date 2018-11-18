627b80442c57b0651dba790bdf2fabe3f0c1eb49
notomo <notomo.motono@gmail.com>
2018-11-18 12:49:51 +0900

    Add tuple interface, function

---
+++ src/scripts/ctrlb/tuple.ts
@@ -0,0 +1,8 @@
+export function tuple<T1>(t1: T1): [T1];
+export function tuple<T1, T2>(t1: T1, t2: T2): [T1, T2];
+export function tuple<T1, T2, T3>(t1: T1, t2: T2, t3: T3): [T1, T2, T3];
+export function tuple(...args: any[]) {
+  return args;
+}
+
+export type Tuple = [] | [any] | [any, any] | [any, any, any];
