fe8092dd357413e1f6178d675554c0a575de0fa9
notomo <notomo.motono@gmail.com>
2018-11-18 12:50:48 +0900

    Add router, validator

---
+++ src/scripts/ctrlb/validator.ts
@@ -0,0 +1,103 @@
+import { Tuple } from "./tuple";
+
+export class Validator {
+  protected readonly REQUIRED_NUMBER = 1;
+  protected readonly OPTIONAL_NUMBER = 2;
+  protected readonly REQUIRED_STRING = "required";
+  protected readonly OPTIONAL_STRING = "optional";
+  protected readonly REQUIRED_BOOLEAN = true;
+  protected readonly OPTIONAL_BOOLEAN = false;
+
+  protected readonly OPTIONALS = [
+    this.OPTIONAL_NUMBER,
+    this.OPTIONAL_STRING,
+    this.OPTIONAL_BOOLEAN,
+  ];
+
+  protected readonly REQUIREDS = [
+    this.REQUIRED_NUMBER,
+    this.REQUIRED_STRING,
+    this.REQUIRED_BOOLEAN,
+  ];
+
+  constructor() {}
+
+  public requiredNumber(): number {
+    return this.REQUIRED_NUMBER;
+  }
+
+  public requiredString(): string {
+    return this.REQUIRED_STRING;
+  }
+
+  public requiredBoolean(): boolean {
+    return this.REQUIRED_BOOLEAN;
+  }
+
+  public optionalNumber(): number | null {
+    return this.OPTIONAL_NUMBER;
+  }
+
+  public optionalString(): string | null {
+    return this.OPTIONAL_STRING;
+  }
+
+  public optionalBoolean(): boolean | null {
+    return this.OPTIONAL_BOOLEAN;
+  }
+
+  protected isRequired(value: string | number | boolean): boolean {
+    return this.REQUIREDS.includes(value);
+  }
+
+  protected isOptional(value: string | number | boolean): boolean {
+    return this.OPTIONALS.includes(value);
+  }
+
+  public to<T extends Tuple>(
+    values: { [index: string]: any },
+    types: T,
+    names: { [P in keyof T]: string }
+  ): T {
+    let results: { [index: string]: any } = {};
+    let i = 0;
+    for (const key of Object.values(names)) {
+      if (typeof types[i] === "undefined") {
+        throw new Error("validationValue must not be null.");
+      }
+      const validationValue = types[i];
+
+      if (!(key in values) && this.isRequired(validationValue)) {
+        throw new Error(key + " is a required argument.");
+      }
+
+      if (
+        (!(key in values) || values[key] === null) &&
+        this.isOptional(validationValue)
+      ) {
+        results[key] = null;
+        continue;
+      }
+
+      const value = values[key];
+      if (value === null) {
+        throw new Error(key + " must not be void.");
+      }
+
+      if (typeof validationValue !== typeof value) {
+        throw new Error(
+          key +
+            "'s value must be " +
+            typeof validationValue +
+            " type, but actual: " +
+            typeof value
+        );
+      }
+
+      results[key] = value;
+      ++i;
+    }
+
+    return [...Object.values(results)] as T;
+  }
+}
