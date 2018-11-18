cdc8e4d2a3679e45ff793496527da0c1328ddab7
notomo <notomo.motono@gmail.com>
2018-11-18 13:18:57 +0900

    Add dependencies

--- src/scripts/ctrlb/di.ts
+++ src/scripts/ctrlb/di.ts
@@ -1,4 +1,12 @@
 import { browser } from "webextension-polyfill-ts";
+import { BookmarkActionGroup } from "./action/bookmark";
+import { HistoryActionGroup } from "./action/history";
+import { WindowActionGroup } from "./action/window";
+import { NavigationActionGroup } from "./action/navigation";
+import { EventActionGroup } from "./action/event";
+import { ApiInfoActionGroup } from "./action/apiInfo";
+import { DownloadActionGroup } from "./action/download";
+import { TabActionGroup } from "./action/tab";
 import { ScrollActionGroup } from "./action/scroll";
 import { ZoomActionGroup } from "./action/zoom";
 import { Router } from "./router";
@@ -6,6 +14,42 @@ import { Validator } from "./validator";
 
 export class Di {
   protected static readonly deps: Deps = {
+    BookmarkActionGroup: () => {
+      const tab = Di.get("TabActionGroup");
+      return new BookmarkActionGroup(tab, browser.bookmarks);
+    },
+    HistoryActionGroup: () => {
+      return new HistoryActionGroup(browser.history);
+    },
+    WindowActionGroup: () => {
+      return new WindowActionGroup(browser.windows);
+    },
+    NavigationActionGroup: () => {
+      return new NavigationActionGroup(browser.tabs);
+    },
+    EventActionGroup: () => {
+      return new EventActionGroup(browser.storage.local);
+    },
+    ApiInfoActionGroup: () => {
+      const actionGroups = {
+        tab: Di.get("TabActionGroup"),
+        bookmark: Di.get("BookmarkActionGroup"),
+        history: Di.get("HistoryActionGroup"),
+        window: Di.get("WindowActionGroup"),
+        navigation: Di.get("NavigationActionGroup"),
+        scroll: Di.get("ScrollActionGroup"),
+        zoom: Di.get("ZoomActionGroup"),
+        event: Di.get("EventActionGroup"),
+        download: Di.get("DownloadActionGroup"),
+      };
+      return new ApiInfoActionGroup(actionGroups);
+    },
+    DownloadActionGroup: () => {
+      return new DownloadActionGroup(browser.downloads);
+    },
+    TabActionGroup: () => {
+      return new TabActionGroup(browser.tabs);
+    },
     ScrollActionGroup: () => {
       return new ScrollActionGroup(browser.tabs);
     },
@@ -22,12 +66,28 @@ export class Di {
   };
 
   protected static readonly cache: DepsCache = {
+    BookmarkActionGroup: null,
+    HistoryActionGroup: null,
+    WindowActionGroup: null,
+    NavigationActionGroup: null,
+    EventActionGroup: null,
+    ApiInfoActionGroup: null,
+    DownloadActionGroup: null,
+    TabActionGroup: null,
     ScrollActionGroup: null,
     ZoomActionGroup: null,
     Router: null,
     Validator: null,
   };
 
+  public static get(cls: "BookmarkActionGroup"): BookmarkActionGroup;
+  public static get(cls: "HistoryActionGroup"): HistoryActionGroup;
+  public static get(cls: "WindowActionGroup"): WindowActionGroup;
+  public static get(cls: "NavigationActionGroup"): NavigationActionGroup;
+  public static get(cls: "EventActionGroup"): EventActionGroup;
+  public static get(cls: "ApiInfoActionGroup"): ApiInfoActionGroup;
+  public static get(cls: "DownloadActionGroup"): DownloadActionGroup;
+  public static get(cls: "TabActionGroup"): TabActionGroup;
   public static get(cls: "ScrollActionGroup"): ScrollActionGroup;
   public static get(cls: "ZoomActionGroup"): ZoomActionGroup;
   public static get(cls: "Router"): Router;
@@ -63,6 +123,14 @@ export class Di {
 }
 
 interface Deps {
+  BookmarkActionGroup: { (...args: any[]): BookmarkActionGroup };
+  HistoryActionGroup: { (...args: any[]): HistoryActionGroup };
+  WindowActionGroup: { (...args: any[]): WindowActionGroup };
+  NavigationActionGroup: { (...args: any[]): NavigationActionGroup };
+  EventActionGroup: { (...args: any[]): EventActionGroup };
+  ApiInfoActionGroup: { (...args: any[]): ApiInfoActionGroup };
+  DownloadActionGroup: { (...args: any[]): DownloadActionGroup };
+  TabActionGroup: { (...args: any[]): TabActionGroup };
   ScrollActionGroup: { (...args: any[]): ScrollActionGroup };
   ZoomActionGroup: { (...args: any[]): ZoomActionGroup };
   Router: { (...args: any[]): Router };
