22af6217f58d9e723c17bf908ee7c99a3ef2b9df
notomo <notomo.motono@gmail.com>
2018-11-23 10:30:08 +0900

    Add a history/remove test

--- src/scripts/ctrlb/action/history.test.ts
+++ src/scripts/ctrlb/action/history.test.ts
@@ -3,13 +3,16 @@ import { History } from "webextension-polyfill-ts";
 
 describe("HistoryActionGroup", () => {
   let search: jest.Mock;
+  let deleteUrl: jest.Mock;
   let actionGroup: HistoryActionGroup;
 
   beforeEach(() => {
     search = jest.fn();
+    deleteUrl = jest.fn();
 
     const HistoryClass = jest.fn<History.Static>(() => ({
       search: search,
+      deleteUrl: deleteUrl,
     }));
     const history = new HistoryClass();
 
@@ -25,5 +28,11 @@ describe("HistoryActionGroup", () => {
   it("search return empty if input is null.", async () => {
     await actionGroup.search();
     expect(search).toHaveBeenCalledWith({ text: "" });
+  });
+
+  it("remove", () => {
+    const url = "url";
+    actionGroup.remove(url);
+    expect(deleteUrl).toHaveBeenCalledWith({ url: url });
   });
 });
