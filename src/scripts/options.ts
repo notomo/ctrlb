49a5b65d53a7cb15c61d17848cb2d8d566aa9321
notomo <notomo.motono@gmail.com>
2018-12-03 21:49:31 +0900

    Clean up
    - remove unneeded type definition
    - add empty lines

--- src/scripts/options.ts
+++ src/scripts/options.ts
@@ -4,12 +4,15 @@ const config = Di.get("Config");
 
 const saveOptions = async () => {
   const hostForm = document.getElementById("host") as HTMLInputElement;
-  const host = hostForm.value as string;
+  const host = hostForm.value;
+
   const statusLabel = document.getElementById("status") as HTMLElement;
+
   await config.saveHost(host).catch(() => {
     statusLabel.textContent = "Failed saving options.";
   });
   statusLabel.textContent = "Options saved.";
+
   setTimeout(() => {
     statusLabel.textContent = "";
   }, 750);
@@ -20,9 +23,10 @@ saveButton.addEventListener("click", saveOptions);
 
 const restoreOptions = async () => {
   const defaultLabel = document.getElementById("default") as HTMLElement;
-  const hostForm = document.getElementById("host") as HTMLInputElement;
   defaultLabel.textContent = config.DEFAULT_HOST;
-  const host: string = await config.getHost();
+
+  const host = await config.getHost();
+  const hostForm = document.getElementById("host") as HTMLInputElement;
   hostForm.value = host;
 };
 document.addEventListener("DOMContentLoaded", restoreOptions);
